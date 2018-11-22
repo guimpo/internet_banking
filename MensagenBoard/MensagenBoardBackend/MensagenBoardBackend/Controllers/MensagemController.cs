b5b6e7892ccd25298191cc674282e10ed922ff11
KaoYuChunG <kaoyuchun14@gmail.com>
2018-11-22 10:45:00 -0200

    bom dia

---
+++ MensagenBoard/MensagenBoardBackend/MensagenBoardBackend/Controllers/MensagemController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MensagenBoardBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/mensagens")]
+    [ApiController]
+    public class MensagemController : ControllerBase
+    {
+        static List<Models.Mensagem> messages = new List<Models.Mensagem> {
+                 new Models.Mensagem
+                {
+                    nome="kaoo",
+                    sobrenome="yuy"
+                },
+                new Models.Mensagem
+                {
+                    nome="Luiz",
+                    sobrenome="lugar"
+                }
+        };
+        public IEnumerable<Models.Mensagem> Get()
+        {
+            return messages;
+        }
+        //adiciona dado
+        [HttpPost]
+        public void Post([FromBody] Models.Mensagem message)
+        {
+            messages.Add(message);
+        }
+    }
+}
---
+++ MensagenBoard/MensagenBoardBackend/MensagenBoardBackend/Models/Mensagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MensagenBoardBackend.Models
+{
+    public class Mensagem
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+    }
+}
