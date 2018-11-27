a43d68e6a030b70678a825484b1ea0cbc66de042
guimpo <nakaima.prk@gmail.com>
2018-11-27 04:57:29 -0800

    Add ContaSalario model

---
+++ backend/BackEnd/Models/ContaSalario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class ContaSalario
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string Agencia { get; set; }
+        public int TipoConta { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public float Saldo { get; set; }
+    }
+}
