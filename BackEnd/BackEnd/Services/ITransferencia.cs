4a66fd5eef3a2da57b7e88130dcd33f254795101
guimpo <nakaima.prk@gmail.com>
2018-11-27 18:21:02 -0800

    Add ITransferencia

---
+++ BackEnd/BackEnd/Services/ITransferencia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Services
+{
+    public interface ITransferencia<T>
+    {
+        T Transferir(T origem, T destino, float valor);
+    }
+}
