293ced4d44400207b924f4a8ecd95601b871ef55
guimpo <nakaima.prk@gmail.com>
2018-11-27 22:51:34 -0800

    Add Deposito method service

---
+++ BackEnd/BackEnd/Dto/DepositoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Dto
+{
+    public class DepositoDto
+    {
+        public int IdContaSalario { get; set; }
+        public float Valor { get; set; }
+    }
+}
