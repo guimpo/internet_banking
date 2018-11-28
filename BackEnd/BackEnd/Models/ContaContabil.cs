74c7aca877910f965d4b5d32bcaffb68e99e52d7
guimpo <nakaima.prk@gmail.com>
2018-11-27 18:21:32 -0800

    Add ContaContabil

---
+++ BackEnd/BackEnd/Models/ContaContabil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class ContaContabil
+    {
+        public int Id { get; set; } 
+        public Pessoa Pessoa { get; set; }
+        public Produto Produto { get; set; }
+        public DateTime DataHora { get; set; }
+        public int TipoTransacao { get; set; }
+        public float Saldo { get; set; }
+    }
+}
