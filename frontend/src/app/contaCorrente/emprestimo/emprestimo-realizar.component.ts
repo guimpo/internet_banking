144ae7cc1ff24406b9e1bd87cb38e7ddadcf187c
marceloozawa <hakozawa@gmail.com>
2018-11-30 16:17:02 -0200

    Alterações front -> empréstimo component

--- frontend/src/app/contaCorrente/emprestimo/emprestimo-realizar.component.ts
+++ frontend/src/app/contaCorrente/emprestimo/emprestimo-realizar.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
-
+import { WebService } from '../../web.service';
 
 @Component({
     selector: 'emprestimo-realizar',
     templateUrl: '../../views/emprestimo/emprestimo-realizar.html',
 })
 
-export class EmprestimoRealizarComponent {}
+export class EmprestimoRealizarComponent {
+    constructor(private webService : WebService){}
+
+    conta;
+    emprestimo = {
+        valor: 100,
+        parcelas: 12
+    }
+
+    async ngOnInit() {
+        //recebe do servidor
+        var response = await this.webService.getLogin(1);
+        console.log(response.json());
+  
+        //vai mostrar la em cima cooresponte
+        this.conta = response.json();
+    }
+}
