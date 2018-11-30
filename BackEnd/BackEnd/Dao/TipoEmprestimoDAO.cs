a426a3f2e54ab8d83f07b40273c5305a38b48627
AmandaSao <saofhama@gmail.com>
2018-11-30 18:12:25 -0200

    tipoemprestimoDAO

---
+++ BackEnd/BackEnd/Dao/TipoEmprestimoDAO.cs
@@ -0,0 +1,70 @@
+﻿using BackEnd.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Dao
+{
+    public class TipoEmprestimoDAO : IDao<TipoEmprestimo>
+    {
+        public TipoEmprestimo Alterar(TipoEmprestimo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoEmprestimo BuscarPorId(int id)
+        {
+            Conexao conexao = new Conexao();
+            try
+            {
+                string comando = "select * from tipo_emprestimo where id = @id";
+                conexao.Comando.CommandText = comando;
+                conexao.Comando.Parameters.AddWithValue("@id", id);
+                MySqlDataReader reader = conexao.Comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    TipoEmprestimo tipoemprestimo = new TipoEmprestimo()
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Juros_Atrasado = Convert.ToDouble(reader["juros_atrasado"]),
+                        Juros_Total = Convert.ToDouble(reader["juros_total"])
+
+                    };
+
+                    return tipoemprestimo;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (MySqlException e)
+            {
+
+                return null;
+            }
+            finally
+            {
+                conexao.Fechar();
+            }
+        }
+
+        public TipoEmprestimo Deletar(TipoEmprestimo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoEmprestimo Inserir(TipoEmprestimo t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoEmprestimo> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
