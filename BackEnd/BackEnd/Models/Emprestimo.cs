0a4dbc1bfdcb4014d8ef1685fd09ba59991ff198
AmandaSao <saofhama@gmail.com>
2018-11-30 13:53:49 -0200

    criei emprestimo

--- BackEnd/BackEnd/Models/Emprestimo.cs
+++ BackEnd/BackEnd/Models/Emprestimo.cs
@@ -8,6 +8,10 @@ namespace BackEnd.Models
     public class Emprestimo
     {
         public int Id { get; set; }
-
+        public Double valor { get; set; }
+        public DateTime data_solicitacao { get; set; }
+        public TipoEmprestimo TipoEmprestimo { get; set; }
+        public ContaContabilEmprestimo ContaContabilEmprestimo { get; set; }
+        public Conta Conta { get; set; }
     }
 }
