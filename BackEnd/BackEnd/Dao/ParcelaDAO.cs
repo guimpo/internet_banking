26184bc088695ad7c9058d97f1c6de9958a7db4b
AmandaSao <saofhama@gmail.com>
2018-11-30 17:45:21 -0200

    ListartodosID

--- BackEnd/BackEnd/Dao/ParcelaDAO.cs
+++ BackEnd/BackEnd/Dao/ParcelaDAO.cs
@@ -81,5 +81,45 @@ namespace BackEnd.Dao
         {
             throw new NotImplementedException();
         }
+
+        public List<Parcela> ListarTodosID()
+        {
+            Conexao conexao = new Conexao();
+            try
+            {
+
+                List<Models.Parcela> parcelas = new List<Models.Parcela> { };
+
+
+                string comando = "SELECT p.data_vencimento AS data_vencimento, p.valor AS valor, p.status as status FROM parcela p inner JOIN emprestimo e ON p.emprestimo_id = e.id where e.conta_id = 7";
+                conexao.Comando.CommandText = comando;
+                MySqlDataReader reader = conexao.Comando.ExecuteReader();
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        Parcela p = new Models.Parcela
+                        {
+                            DataVencimento = Convert.ToDateTime(reader["data_vencimento"]),
+                            Status = (reader["status"].ToString().Equals("1")) ? true : false,
+                            Valor = Convert.ToDouble(reader["valor"])
+                        };
+                        parcelas.Add(p);
+                    }
+
+                }
+                return parcelas;
+
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            finally
+            {
+                conexao.Fechar();
+            }
+        }
     }
 }
