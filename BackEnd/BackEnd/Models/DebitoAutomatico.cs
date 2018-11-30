4c1c68e1640e61ea8c02eee635eab87c162b796c
guimpo <nakaima.prk@gmail.com>
2018-11-29 17:56:32 -0800

    Removida classe ContaCadastro, adicionado DebitoAutomatico

---
+++ BackEnd/BackEnd/Models/DebitoAutomatico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class DebitoAutomatico
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Codigo { get; set; }
+        public Conta Conta { get; set; }
+    }
+}
