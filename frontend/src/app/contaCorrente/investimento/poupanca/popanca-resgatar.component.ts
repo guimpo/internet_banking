5c9a49ba887d9c403518f4322e9cbb973f408909
Lukas-diniz <lukas-diniz@hotmail.com>
2018-12-03 11:51:04 -0200

    alterações investimento

--- frontend/src/app/contaCorrente/investimento/poupanca/popanca-resgatar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-
-@Component({
-    selector: 'investimento-popanca-Resgatar',
-    templateUrl: '../../../views/investimento/poupanca/popanca-resgatar.html',
-})
-
-export class PopancaResgatarComponent {}
