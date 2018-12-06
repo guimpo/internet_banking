5296da20d3e2d6cebbf26e0d4c5297fc1a9a15a0
guimpo <nakaima.prk@gmail.com>
2018-12-06 02:03:26 -0200

    Tesouro prefixado ltn controller

---
+++ BackEnd/BackEnd/Controllers/TesouroPrefixadoLtnController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/investimento/[controller]")]
+    [ApiController]
+    public class TesouroPrefixadoLtnController : ControllerBase
+    {
+        // GET: api/investimento/tesouroprefixadoltn
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Controller", "works!" };
+        }
+    }
+}
