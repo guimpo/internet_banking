68ca54ba31cc5d6fff519e7aa87e95c23b889a85
Paulo Nakaima <nakaima.prk@gmail.com>
2018-12-07 03:47:47 -0200

    Adicionado variavel de ambiente

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// 'https://localhost:44397/api'
+// 'https://localhost:5001/api'
 export const environment = {
   production: false,
   API: 'https://localhost:44397/api'
